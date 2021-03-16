62bf14883d40f84d0cb558884e40380586809b5a
Alejandro-DP <alexdelacruz1888@gamil.com>
2021-03-15 21:22:27 -0600

    iteracion de tabla , y css

--- jjs/app.js
+++ jjs/app.js
@@ -1,23 +1,24 @@
-const listvalore = document.getElementById('tabla');
+const listvalore = document.getElementById('data');
 function Number(){
     var numero2 = null;
     var numero1 = null;
     numero1 = document.getElementsByName("valor1")[0].value;
     numero2 = document.getElementsByName("valor2")[0].value;
-    const td = document.createElement('td');
+    const tr = document.createElement('tr');
    
 
-    let ErrorA = (numero1 - numero2).toFixed(3);
+    let ErrorA = (numero1 - numero2).toFixed(5);
     let ErrorR = (ErrorA / numero1).toFixed(6);
     let ErrorP = (ErrorR * 100 ).toFixed(4);
 
 
     
     var list = new Array (ErrorA,ErrorR,ErrorP);
-    
-   for (var i = 0; i < list.length; i++){ 
-    const td = document.createElement('td');
-        td.innerHTML = list[i];
-        listvalore.appendChild(td);  
-   }
+    let tds = "";
+    list.map((li) => {
+        let td = `<td>${li}</td>`;
+        tds = tds + td;
+      });
+      tr.innerHTML = tds;
+      listvalore.appendChild(tr);
 };
